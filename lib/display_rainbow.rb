2e996baee425c734aa883cecbb22e196f3c7bf6b
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-04 17:55:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,4 @@
 
 def display_rainbow(arr)
   puts "R: #{arr[0]}, O #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]}"
-  
-  ("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n")
-    
+end
